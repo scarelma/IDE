f8c0ae1999a1159ceb6d473fb094cc8aa1b81854
mukul-kr <mukul0000kumar@gmail.com>
2023-11-26 17:47:34 +0530

    frontend fe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,59 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Fragment } from "react";
+import Editor from "@monaco-editor/react";
+// import { monaco } from '@monaco-editor/react';
+import loader from '@monaco-editor/loader';
+
+// Configure the source of the Monaco files
+loader.config({
+  paths: {
+    vs: "/vs",
+  },
+
+});
+
+loader.init();
 
 function App() {
   const [code, setCode] = useState("");
   const [input, setInput] = useState("");
   const [language, setLanguage] = useState("python");
   const [output, setOutput] = useState("");
+  const [languageList, setLanguageList] = useState([]);
 
-  // language="python"
+
+  const options = {
+    autoIndent: 'full',
+    contextmenu: true,
+    fontFamily: 'monospace',
+    fontSize: 13,
+    lineHeight: 24,
+    hideCursorInOverviewRuler: true,
+    matchBrackets: 'always',
+    minimap: {
+      enabled: true,
+    },
+    scrollbar: {
+      horizontalSliderSize: 4,
+      verticalSliderSize: 18,
+    },
+    selectOnLineNumbers: true,
+    roundedSelection: false,
+    readOnly: false,
+    cursorStyle: 'line',
+    automaticLayout: true,
+  };
+
+  useEffect(() => {
+    fetch("http://localhost/api/v1/language", {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setLanguageList(data);
+      });
+  }, []);
 
 
   let handleSubmit = async (e) => {
@@ -20,94 +65,81 @@ function App() {
       }, 1000);
       return;
     }
-    var res
-    var languageList
-    var langid
-    try {
-      await fetch("/api/v1/language", {
-        method: "GET",
-        mode: "no-cors",
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          languageList = data;
-        });
-      console.log(languageList);
-    } catch (error) {
-      console.log(error);
-    }
-    console.log(languageList.length);
-    for (var i = 0; i < languageList.length; i++) {
-      if (languageList[i].Name === language) {
-        langid = languageList[i].Id;
-        console.log(langid)
+    let langid;
+    languageList.forEach(obj => {
+      if (obj.Name === language) {
+        langid = obj.Id;
+        console.log(langid);
       }
-    }
+    });
+    console.log(langid);
     var raw = JSON.stringify({
       langnumber: langid,
       code,
       input,
-    })
+    });
 
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    myHeaders.append('Access-Control-Allow-Origin', '*');
+    myHeaders.append("Access-Control-Allow-Origin", "*");
 
-    res = await fetch("/api/v1/sendcode", {
+    var res = await fetch("http://localhost/api/v1/sendcode", {
       method: "POST",
       headers: myHeaders,
       body: raw,
       mode: "cors",
-      // mode: "no-cors",
-      redirect: 'follow',
+      redirect: "follow",
     });
 
     let resJson = await res.json();
     console.log(resJson);
     setOutput(resJson.output);
-  }
+  };
 
   const handleLanguageChange = (e) => {
     setLanguage(e.target.value);
-    console.log(e.target.value)
   };
 
-  const handleCodeChange = (e) => {
-    setCode(e.target.value);
+  const handleCodeChange = (value, event) => {
+    setCode(value);
   };
 
-  const handleInputChange = (e) => {
-    setInput(e.target.value);
-  }
-
+  const handleInputChange = (value, event) => {
+    setInput(value);
+  };
 
   return (
     <div className="App">
-      {/* a headline stating it is python coding enviourment */}
       <h1>Coding Environment</h1>
-      <form >
-
-        <select onChange={handleLanguageChange} >
-
+      <form>
+        <select onChange={handleLanguageChange}>
           <option value="python">Python</option>
           <option value="javascript">Javascript</option>
           <option value="java">Java</option>
-          <option value="c++">C++</option>
+          <option value="cpp">C++</option>
           <option value="go">Go</option>
         </select>
         <br />
-        <div className="editor">
-          <div className="line-numbers">
-            <span></span>
-          </div>
-          <textarea name="code"
-            value={code}
-            id="codingArea"
-            placeholder="code"
-            onChange={handleCodeChange}
-            required
-            cols="40"
-            rows="5"></textarea>
+        <div className="editor"  >
+          <center >
+            <div style={{ border: '1px solid red', height: "500px", width: "950px" }}>
+
+              <Editor
+                name="code"
+                id="codingArea"
+                value={code}
+                onChange={handleCodeChange}
+                required
+                height="500px"
+                // theme="vs-dark"
+                width="950px"
+                defaultLanguage="python"
+                language={language}
+                // defaultValue="// you can write code here //"
+                options={options}
+              />
+            </div>
+          </center>
         </div>
 
         <br />
@@ -119,7 +151,8 @@ function App() {
           onChange={handleInputChange}
           required
           cols="40"
-          rows="5"></textarea>
+          rows="5"
+        ></textarea>
         <br />
 
         <label htmlFor="upload">Send Code</label>
@@ -128,24 +161,20 @@ function App() {
       </form>
       <center>
         <div className="message">
-          {output ? <p>{
-            output
-              .split(/\n/)
-              .map(
-                line => <Fragment key={line}>
+          {output ? (
+            <p>
+              {output.split(/\n/).map((line) => (
+                <Fragment key={line}>
                   {line}
                   <br />
                 </Fragment>
-              )
-          }
-          </p> : null
-          }
+              ))}
+            </p>
+          ) : null}
         </div>
       </center>
-
     </div>
   );
-
 }
 
 export default App;
